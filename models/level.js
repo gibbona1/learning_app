fc35eb2bf5a73cb9648a177b61fd6eb881bcc151
gibbona1 <33295389+gibbona1@users.noreply.github.com>
2024-03-16 21:44:41 +0000

    update schema

--- models/level.js
+++ models/level.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const levelSchema = new mongoose.Schema({
     levelNumber: { type: Number, required: true, unique: true },
     description: { type: String },
-    unlocksAt: { type: Number } // e.g., total points needed to unlock
   });
   
 const Level = mongoose.model('Level', levelSchema);
--- models/progress.js
+++ models/progress.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const progressSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     birdCallId: { type: mongoose.Schema.Types.ObjectId, ref: 'BirdCall', required: true },
-    level: { type: Number, default: 0 },
+    level: { type: Number, default: 1 }, //this will be apprentice 1-4, guru etc like WaniKani
     lastReviewed: { type: Date, default: Date.now },
     nextReviewDate: { type: Date, required: true }
   });
