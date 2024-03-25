5e4ed2e931379c2b641d5541a4de58fd29c402fa
gibbona1 <33295389+gibbona1@users.noreply.github.com>
2024-03-25 12:09:58 +0000

    Add back routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
-//import UserPage from './components/UserPage';
-//import BirdCallsPage from './components/BirdCallsPage';
-//import UserLevelsPage from './components/UserLevelsPage';
+import UserPage from './components/UserPage';
+import BirdCallsPage from './components/BirdCallsPage';
+import UserLevelsPage from './components/UserLevelsPage';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route index element={<HomePage />} />
+        <Route path="/users" element={<UserPage />} />
+        <Route path="/birdcalls" element={<BirdCallsPage />} />
+        <Route path="/userlevels" element={<UserLevelsPage />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
