700d596b4b76b90b3599be369ed7799144c1177c
gibbona1 <33295389+gibbona1@users.noreply.github.com>
2024-03-27 21:22:47 +0000

    fix audio file load from S3 bucket

--- client/src/components/ReviewSessionPage.js
+++ client/src/components/ReviewSessionPage.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { FetchHttpHandler } from "@smithy/fetch-http-handler";
 const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
-const { writeFileSync } = require('fs');
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
 export default function ReviewSession() {
   const location = useLocation();
   const currentUserId = location.state.userId;
   const [mergedData, setMergedData] = useState([]);
+  const [signedUrl, setSignedUrl] = useState("");
 
   const [currentReviewIndex, setCurrentReviewIndex] = useState(0);
   useEffect(() => {
@@ -54,72 +54,32 @@ export default function ReviewSession() {
     setCurrentReviewIndex(prevIndex => prevIndex + 1);
   }
 
-  const client = new S3Client({region: process.env.REACT_APP_AWS_DEFAULT_REGION,
-    credentials: {accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-                  secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY
-     }});
-  async function downloadObject(bucketName, objectKey, outputPath) {
-      try {
-          const response = await client.send(new GetObjectCommand({
-              Bucket: bucketName,
-              Key: objectKey,
-          }));
-          alert("here");
-          const bodyContent = await streamToBuffer(response.Body);
-          writeFileSync(outputPath, bodyContent);
-          alert(`File downloaded successfully to ${outputPath}`);
-      } catch (err) {
-          alert("Error downloading file:", err);
-      }
-  }
-
-  function streamToBuffer(stream) {
-    return new Promise((resolve, reject) => {
-        const chunks = [];
-        stream.on('data', (chunk) => chunks.push(chunk));
-        stream.on('end', () => resolve(Buffer.concat(chunks)));
-        stream.on('error', reject);
-    });
-  }
-
   const currentReview = mergedData[currentReviewIndex];
 
-  function getObject () {
-    return new Promise(async (resolve, reject) => {
-
-      const client = new S3Client({region: process.env.REACT_APP_AWS_DEFAULT_REGION,
-        credentials: {accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-                      secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY
-         },
-        requestHandler: new FetchHttpHandler({ keepAlive: false })});
-
+  useEffect(() => {
+    async function makeSignedUrl(bucketName, objectKey) {
       try {
-        const response = await client.send(new GetObjectCommand({ 
-                                            Bucket: "my-audio-bucket-2024", 
-                                            Key: "CARNSOREMET_20220707_080200_93_96.wav" }));
-        const buffer = Buffer.from(await response.Body.transformToByteArray())
-        return buffer;
+          const client = new S3Client({region: process.env.REACT_APP_AWS_DEFAULT_REGION,
+            credentials: {accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+                          secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY
+            }});
+          const command = new GetObjectCommand({
+              Bucket: bucketName,
+              Key: objectKey,
+          });
+          const signedUrl = await getSignedUrl(client, command, { expiresIn: 3600 });
+          alert("Signed URL:", JSON.stringify(signedUrl));
+          setSignedUrl(signedUrl);
       } catch (err) {
-        // Handle the error or throw
-        reject(err);
-      } 
-    })
-  }
-
-  async function fetchObject() {
-    try {
-      const result = await getObject();
-      //alert(result); // Handle the resolved value
-    } catch (error) {
-      alert(error); // Handle any errors
+          alert("Error downloading file:", err);
+          return '';
+      }
     }
-  }
+    makeSignedUrl("my-audio-bucket-2024", "CARNSOREMET_20220707_080200_93_96.wav");
+  }, [currentReview]);
 
   if(currentReview) {
-    //alert("currentReview: " + JSON.stringify(currentReview));
     //let signed_url = 
-    //downloadObject("my-audio-bucket-2024", "CARNSOREMET_20220707_080200_93_96.wav", "public/downloaded_file.wav");
-    //alert("got audio");
   } else {
     console.log("No current review");
   }
@@ -131,7 +91,7 @@ export default function ReviewSession() {
           <h3>{currentReview.birdCallData.name}</h3>
           <p>Class: {currentReview.birdCallData.class}</p>
           <p>Level: {currentReview.birdCallData.level}</p>
-          <audio controls src={"https://my-audio-bucket-2024.s3.eu-north-1.amazonaws.com/CARNSOREMET_20220707_080200_93_96.wav?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAU6GDUXRVNJ2SNGVI%2F20240327%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Date=20240327T204653Z&X-Amz-Expires=3600&X-Amz-Signature=417f011ec66d28e8a3a4dcdcc56b6708863e4d3132dd890d764693473431c3b7&X-Amz-SignedHeaders=host&x-id=GetObject"}>
+          <audio controls src={signedUrl}>
             Your browser does not support the audio element.
           </audio>
           <button onClick={goToNextReview}>Next Review</button>
