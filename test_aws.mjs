55366fd7b1f0c553de4fd404655f5a6d6f54ec1b
gibbona1 <33295389+gibbona1@users.noreply.github.com>
2024-03-27 20:46:04 +0000

    change download audio to signed url

--- test_aws.mjs
+++ test_aws.mjs
@@ -1,33 +1,25 @@
 import {GetObjectCommand, S3Client} from "@aws-sdk/client-s3";
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-const { writeFileSync } = require('fs');
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+
 
 const client = new S3Client({region: process.env.REACT_APP_AWS_DEFAULT_REGION,
     credentials: {accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
                   secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY
      }});
-async function downloadObject(bucketName, objectKey, outputPath) {
+async function makeSignedUrl(bucketName, objectKey, outputPath) {
     try {
-        const response = await client.send(new GetObjectCommand({
+        const command = new GetObjectCommand({
             Bucket: bucketName,
             Key: objectKey,
-        }));
-        const bodyContent = await streamToBuffer(response.Body);
-        writeFileSync(outputPath, bodyContent);
-        console.log(`File downloaded successfully to ${outputPath}`);
+        });
+        const signedUrl = await getSignedUrl(client, command, { expiresIn: 3600 });
+        console.log("Signed URL:", signedUrl);
+        return signedUrl;
     } catch (err) {
         console.error("Error downloading file:", err);
     }
 }
 
-function streamToBuffer(stream) {
-    return new Promise((resolve, reject) => {
-        const chunks = [];
-        stream.on('data', (chunk) => chunks.push(chunk));
-        stream.on('end', () => resolve(Buffer.concat(chunks)));
-        stream.on('error', reject);
-    });
-}
-
-downloadObject("my-audio-bucket-2024", "CARNSOREMET_20220707_080200_93_96.wav", "./downloaded_file.wav");
+makeSignedUrl("my-audio-bucket-2024", "CARNSOREMET_20220707_080200_93_96.wav", "./downloaded_file.wav");
