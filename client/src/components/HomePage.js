da2363476a9e475d77a425613feed85cbe3e043e
gibbona1 <33295389+gibbona1@users.noreply.github.com>
2024-03-28 15:39:49 +0000

    update chart to review count data

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import NavBar from './NavBar';
 import {
   Chart as ChartJS,
@@ -33,31 +33,54 @@ export const options = {
   },
 };
 
-const labels = ['January', 'February', 'March'];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: [10,20,30],
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
-      label: 'Dataset 2',
-      data: [15, 15, 15],
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
-  ],
-};
-
 export default function HomePage() {
+  const currentUserId = '65fcc504b999225e008c71c5';
+  const [countData, setCountData] = useState("");
+
+  useEffect(() => {
+    fetch(`/api/itemsgetbyhour/${currentUserId}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      setCountData(data); // Ensure this is an array
+      return data; // Ensure this is an array
+    })
+    .catch (error => {
+    console.error('Error fetching reviews:', error);
+    });
+  }, [currentUserId]);
+
+  //alert(JSON.stringify(countData));
+    // Assuming you're using ES modules, otherwise adjust the import according to your environment
+  //get hour of current time mod 24
+  const currentHour = new Date().getHours(); // Note: This gets the current hour in local time
+
+  const labels = countData.map((item) => (item.hour + currentHour) % 24);
+  const data   = countData.map((item) => item.count);
+
+  const chartData = {
+    labels: labels,
+    datasets: [
+      {
+        label: 'Item Count',
+        data: data,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1,
+      },
+    ],
+  }
+
   return (
   <div>
     <NavBar />
     <h1>Home Page</h1>
     <p>Welcome to our application!</p>
-    <Bar options={options} data={data} />
+    <Bar options={options} data={chartData} />
   </div>
   );
 }
