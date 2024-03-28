c58f96ae749627addcbdc9a13a160e7548e92d8f
gibbona1 <33295389+gibbona1@users.noreply.github.com>
2024-03-28 15:52:33 +0000

    change x axis to real time

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -36,6 +36,7 @@ export const options = {
 export default function HomePage() {
   const currentUserId = '65fcc504b999225e008c71c5';
   const [countData, setCountData] = useState("");
+  const [chartData, setChartData] = useState({});
 
   useEffect(() => {
     fetch(`/api/itemsgetbyhour/${currentUserId}`)
@@ -54,33 +55,37 @@ export default function HomePage() {
     });
   }, [currentUserId]);
 
-  //alert(JSON.stringify(countData));
-    // Assuming you're using ES modules, otherwise adjust the import according to your environment
-  //get hour of current time mod 24
-  const currentHour = new Date().getHours(); // Note: This gets the current hour in local time
+  useEffect(() => {
+    if(!countData) {
+      setChartData({});
+    } else {
+      const currentHour = new Date().getHours(); // Note: This gets the current hour in local time
 
-  const labels = countData.map((item) => (item.hour + currentHour) % 24);
-  const data   = countData.map((item) => item.count);
+      const labels = countData.map((item) => (item.hour + currentHour) % 24);
+      const data   = countData.map((item) => item.count);
 
-  const chartData = {
-    labels: labels,
-    datasets: [
-      {
-        label: 'Item Count',
-        data: data,
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        borderColor: 'rgba(255, 99, 132, 1)',
-        borderWidth: 1,
-      },
-    ],
-  }
+      const d = {
+        labels: labels,
+        datasets: [
+          {
+            label: 'Item Count',
+            data: data,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+          },
+        ],
+      }
+      setChartData(d);
+    }
+  }, [countData]);
 
   return (
   <div>
     <NavBar />
     <h1>Home Page</h1>
     <p>Welcome to our application!</p>
-    <Bar options={options} data={chartData} />
+    {countData ? (<Bar options={options} data={chartData} />) : (<p>Loading...</p>)}
   </div>
   );
 }
