1b4af222b8af79a664e05c77d0a7105704372a8a
gibbona1 <33295389+gibbona1@users.noreply.github.com>
2024-04-12 14:14:55 +0100

    longest streak

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -1,5 +1,39 @@
 const Session = require('../models/session');
 
+function getLongestStreak(sessions) {
+    try {
+        let maxStreak = 0;
+        let currentStreak = 1;
+        let previousDate = null;
+
+        //sort sessions by startTime
+        sessions.sort((a, b) => a.startTime - b.startTime);
+
+        sessions.forEach((session, index) => {
+            if (index === 0) {
+                previousDate = session.startTime;
+                return;
+            }
+            let currentDate = session.startTime;
+            let diff = currentDate.getDate() - previousDate.getDate();
+            if (diff === 1 || (diff === 0 && currentDate.getDay() !== previousDate.getDay())) {
+                currentStreak++;
+            } else if (diff > 1) {
+                maxStreak = Math.max(maxStreak, currentStreak);
+                currentStreak = 1;
+            }
+            previousDate = currentDate;
+        });
+
+        maxStreak = Math.max(maxStreak, currentStreak); // Check at the end of the loop
+        return maxStreak;
+    } catch (error) {
+        console.error('Error calculating the longest streak: ', error);
+        return 0;
+    }
+}
+
+
 exports.createSession = async (req, res) => {
   try {
     const { userId } = req.body;
@@ -59,18 +93,30 @@ exports.getStreak = async (req, res) => {
   try {
     const { userId } = req.params;
     const sessions = await Session.find({ userId }).sort({ startTime: 'desc' });
+
+    const uniqueSessions = sessions.filter((session, index, self) => {
+      return index === self.findIndex((t) => (
+        t.startTime.getDate() === session.startTime.getDate() &&
+        t.startTime.getMonth() === session.startTime.getMonth() &&
+        t.startTime.getFullYear() === session.startTime.getFullYear()
+      ));
+    });
+
     let streak = 0;
-    let currentDay = new Date().getDay();
-    sessions.forEach(session => {
-      const sessionDay = new Date(session.startTime).getDay();
-      if (currentDay === sessionDay) {
+    // sessions sort newest to oldest
+    let currentDate = new Date().getDate();
+    uniqueSessions.forEach(session => {
+      const sessionDate = new Date(session.startTime).getDate();
+      if (currentDate === sessionDate) {
         streak++;
-        currentDay--;
+        currentDate--;
       } else {
         return;
       }
     });
-    res.status(200).json({ streak });
+    const maxStreak = getLongestStreak(uniqueSessions);
+    
+    res.status(200).json({ streak: streak, maxStreak: maxStreak});
   } catch (error) {
     res.status(500).json({ message: 'Error fetching streak', error: error.message });
   }
