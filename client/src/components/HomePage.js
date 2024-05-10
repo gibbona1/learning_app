3d96a93bbc6eca520baec69754159deea030cd42
gibbona1 <33295389+gibbona1@users.noreply.github.com>
2024-05-10 13:07:41 +0100

    initial levelup button

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from './NavBar';
+import { handleResponse } from './helpers';
 
 export default function HomePage({ isAuth, setAuth, userId, setUserId, userRole, setUserRole, userLevel, setUserLevel, sessionId, setSessionId}) {
   const logout = () => {
@@ -16,12 +17,29 @@ export default function HomePage({ isAuth, setAuth, userId, setUserId, userRole,
     setSessionId(null);
   };
 
+  function startLevelUp() {
+    fetch(`api/users/${userId}/levelUp`, {
+      method: 'PUT'
+    })
+    .then(handleResponse)
+    .then(data => {
+      alert('LevelUp completed successfully:', data);
+      // reload page
+      setUserLevel(userLevel + 1);
+      window.location.reload();
+    })
+    .catch(error => {
+      alert('Error starting levelup:', error);
+    });
+  };
+
   return (
     <div>
       <NavBar />
       {isAuth && <button onClick={logout}>Logout</button>}
       {isAuth && <p>Logged in as user: <b>{userId}</b>. role: <b>{userRole}</b></p>}
       {isAuth && <p>Level: <b>{userLevel}</b></p>}
+      {isAuth && userLevel === 0 && <button onClick={startLevelUp}>Unlock Level 1</button>}
       <h1>Home Page</h1>
       <p>Welcome to our application!</p>
     </div>
